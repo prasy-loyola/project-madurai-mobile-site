c59a48adf96a421289b40c33e2341f32959754c0
Prasanna Selvaraj <prasy.loyola@gmail.com>
2020-12-18 23:28:40 +0800

    added font awesome and removed unwanted pages

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.write('<script src="./js/general-utils.js"></script>');
-document.write('<script src="./js/books-utils.js"></script>');
-document.write('<script src="./js/components.js"></script>');
-document.write('<script src="./js/book-list-view.js"></script>');
+$('<script src="./js/general-utils.js"></script>').appendTo(document.body);
+$('<script src="./js/books-utils.js"></script>').appendTo(document.body);
+$('<script src="./js/components.js"></script>').appendTo(document.body);
+$('<script src="./js/book-list-view.js"></script>').appendTo(document.body);
